6f0d41178de0f334f7dc42d8131a6d948e1342da
Joy Antioho <j.wildfong@yahoo.com>
2026-07-07 08:12:42 -0400

    advanced_portfolio

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// Wl3X2dQEgwt21sBFA
+
+// function contact(event) {
+//     event.preventDefault();
+//     emailjs
+//        .sendForm(
+//         'service_d38h55g'
+//         'template_s3chdi9'
+//         event.target
+//         'Wl3X2dQEgwt21sBFA'
+//        ).then(() => {
+//         console.log('this worked1')
+//        })
+setTimeout(() => {
+    console.log('it worked 1')
+}, 500);
+}
