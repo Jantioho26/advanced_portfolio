5894fdcc7300f966a815a3493cd35de8c95f59df
Joy Antioho <j.wildfong@yahoo.com>
2026-07-07 13:08:17 -0400

    advanced_portfolio

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+// template_y8sma54 
+// service_s4tl5r6
+// tq5yOef37QZrOgoN3
 
-
-
-
-function contact(event) {
+function contact() {
     event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible";
-    emailjs
-    .sendForm(
-        'service_s4tl5r6',
-        'template_m31dp54',
-        event.target,
-        'user_tq5yOef37QZrOgoN3'
-    ).then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-    }).catch(() => {
-        console.log("EmailJS. error:", error)
-    })
-}
-
-
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+      emailjs
+      .sendForm(
+         'service_s4tl5r6',
+         'template_y8sma54',
+          event.target,
+         'tq5yOef37QZrOgoN3'
+           ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+          }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on joyantioho@gmail.com"
+           );
+         })
+       }
