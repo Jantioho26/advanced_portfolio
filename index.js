22f42c853cbbcddbea19d7fcaa43a40a066fdca7
Joy Antioho <j.wildfong@yahoo.com>
2026-07-08 07:33:37 -0400

    advanced_portfolio

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
-// template_y8sma54 
-// service_s4tl5r6
-// tq5yOef37QZrOgoN3
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
 
-function contact() {
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+   
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd= i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+
+}
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList.add("dark-theme");
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visible"
-      emailjs
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList.add("modal__overlay--visible");
+    emailjs
       .sendForm(
          'service_s4tl5r6',
          'template_y8sma54',
           event.target,
          'tq5yOef37QZrOgoN3'
-           ).then(() => {
+      ).then(() => {
             loading.classList.remove("modal__overlay--visible");
-            success.classList += " modal__overlay--visible";
-          }).catch(() => {
+            success.classList.add("modal__overlay--visible");
+      }).catch(() => {
             loading.classList.remove("modal__overlay--visible");
             alert(
                 "The email service is temporarily unavailable. Please contact me directly on joyantioho@gmail.com"
            );
-         })
-       }
+      });
+}
 
-        let isModalOpen = false;
-       function toggleModal() {
-        if (isModalOpen) {
-            isModalOpen = false;
-            return document.body.classList.remove("modal--open");
-        }
-        isModalOpen = true;
-       document.body.classList += " modal--open";
-       }
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList.add("modal--open");
+}
